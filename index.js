4df10986c1b99de887f57e6a000bf52501411b97
Krishna <krishnapsnambiar@gmail.com>
2021-02-25 11:29:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
+//functionality
 client.on('message', message => {
     if(message.channel.name == 'walls-and-aesthetic'){
         if (message.attachments.size <= 0) {
